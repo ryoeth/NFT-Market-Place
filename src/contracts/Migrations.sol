66c979fedcd774e94bef5cbe70dd62856c5d8e94
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-03-29 18:04:08 +0530

    migration setup

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,5 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+// Migrations are JavaScript files that help you deploy contracts to the Ethereum network. 
+// These files are responsible for staging your deployment tasks, and they're written under 
+// the assumption that your deployment needs will change over time. As your project evolves, 
+// you'll create new migration scripts to further this evolution on the blockchain. A history 
+// of previously run migrations is recorded on-chain through a special Migrations contract, 
+// detailed below.
+
+pragma solidity 0.8.0;
 
 contract Migrations {
   address public owner = msg.sender;
@@ -16,6 +23,7 @@ contract Migrations {
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
+
 
   function upgrade(address new_address) public restricted {
     Migrations upgraded = Migrations(new_address);
--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "0.6.0",
+      version: "^0.8.0",
       optimizer: {
         enabled: true,
         runs: 200
