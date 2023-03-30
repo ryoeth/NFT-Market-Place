07acee2e9847d7eddef4b94b2bf09c6a4489873e
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-03-30 19:17:14 +0530

    ERC721 MINTING

---
+++ src/contracts/ERC721Connector.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC721Metadata.sol";
+
+contract ERC721Connector is ERC721Metadata {
+    // constructor that takes in the same two string parameters as the 
+    // ERC721Metadata constructor and passes them to the ERC721Metadata 
+    // constructor using the ERC721Metadata(_name, _symbol) syntax.
+    constructor(string memory _name, string memory _symbol) ERC721Metadata(_name, _symbol){
+
+    }
+}
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "./ERC721Connector.sol";
 
-contract KryptoBirdz {
-    string public name;
-    string public symbol;
+contract KryptoBirdz is ERC721Connector {
+    // a constructor that calls the ERC721Connector constructor with the values "Kryptobird"
+    // and "KBIRDZ" to set the name and symbol variables for this contract.
+    constructor() ERC721Connector("Kryptobird", "KBIRDZ"){
 
-    constructor() {
-        name = "Kryptobird";
-        symbol = "KBIRDZ";
     }
 }
