9c69d5c2f560629e4c0a5ff98cb6c299c24a8136
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-04-06 19:35:21 +0530

    _exists function in KryptoBirdz minting

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "./ERC721.sol";
 
 contract ERC721 {
-    mapping(uint => address) private _tokenNumber;
+    mapping(uint => address) private _tokenOwner;
     mapping(address => uint) private _OwnedTokenCounts;
     event Transfer(
         address indexed from,
@@ -14,11 +14,16 @@ contract ERC721 {
 
     function _mint(uint tokenId, address to) internal {
         require(to != address(0), "Address is 0");
-        require(_tokenNumber[tokenId] == address(0),"Token already minted");
+        require(!_exists(tokenId), "Token already minted");
 
-        _tokenNumber[tokenId] = to;
+        _tokenOwner[tokenId] = to;
         _OwnedTokenCounts[to]++;
 
         emit Transfer(address(0), to, tokenId);
+    }
+
+    function _exists(uint tokenId) internal view returns (bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
     }
 }
