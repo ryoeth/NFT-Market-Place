b210d479d254761b59b34d889c45d1960e804e5e
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-04-22 12:37:15 +0530

    truffle compile and tests

--- test/KryptoBirdzTest.js
+++ test/KryptoBirdzTest.js
@@ -1,11 +1,24 @@
-const assert = require('chai');
-const Kryptobird = artifacts.require('./KryptoBird');
+const {assert} = require('chai');
+const Kryptobird = artifacts.require('./KryptoBirdz');
 
 contract('KryptoBirdz', () => {
     let contract;
     describe('deployement', async () => {
         it('deploys successfuly', async () => {
             contract = await Kryptobird.deployed();
+            const address = contract.address;
+            assert.notEqual(address, '');
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+            assert.notEqual(address, 0x0);
+        })
+        it('has a name', async () => {
+            const name = await contract.NameGetter();
+            assert.equal(name, 'Kryptobird');
+        })
+        it('has a symbol', async () => {
+            const symbol = await contract.SymbolGetter();
+            assert.equal(symbol, 'KBIRDZ');
         })
     })
 })
