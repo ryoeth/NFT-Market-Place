4f9e3d0a4c7305de43ef5732241b973f04099141
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-04-27 17:14:17 +0530

    kryptobird testing done

--- test/KryptoBirdzTest.js
+++ test/KryptoBirdzTest.js
@@ -1,11 +1,11 @@
 const { assert } = require('chai');
 const Kryptobird = artifacts.require('./KryptoBirdz');
 
-contract('KryptoBirdz', () => {
-    let contract;
+contract('KryptoBirdz', async (accounts) => {
+    let contract = await Kryptobird.deployed();;
+
     describe('deployement', async () => {
         it('deploys successfuly', async () => {
-            contract = await Kryptobird.deployed();
             const address = contract.address;
             assert.notEqual(address, '');
             assert.notEqual(address, null);
@@ -20,7 +20,8 @@ contract('KryptoBirdz', () => {
             const symbol = await contract.SymbolGetter();
             assert.equal(symbol, 'KBIRDZ');
         })
-    })
+    });
+
     describe('minting', async () => {
         it('creates a new token', async () => {
             const result = await contract.mint('pigeon');
@@ -28,8 +29,28 @@ contract('KryptoBirdz', () => {
             assert.equal(totalSupply, 1);
 
             const event = result.logs[0].args;
-            assert.equal(event.from, 0x000000000000000000000000000000000000000000, 'from = contract addr');
-            assert.equal(event.to, accounts[0], 'to is = msg.sender');
+            assert.equal(event._from, 0x000000000000000000000000000000000000000000, 'from = contract addr');
+            assert.equal(event._to, accounts[0], 'to is = msg.sender');
+
+            // Failure
+            await contract.mint('pigeon').should.be.rejected;
         })
-    })
+    });
+
+    describe('indexing', async () => {
+        it('lists KryptoBirdz', async () => {
+            await contract.mint('peacock');
+            await contract.mint('crow');
+            await contract.mint('myna');
+            const totalSupply = await contract.totalSupply();
+            let result = [];
+            for (i = 1; i <= totalSupply; i++) {
+                let kryptobird = await contract.Kryptobird(i - 1);
+                result.push(kryptobird);
+            }
+            let expected = ['pigeon', 'peacock', 'crow', 'myna'];
+            assert.equal(result.join(','), expected.join(','));
+        });
+    });
+
 })
