ff3aebf511645397f737e6984a46fc3121b4f391
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-04-29 18:28:25 +0530

    loading network address

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useEffect } from 'react'
 import detectEthereumProvider from '@metamask/detect-provider'
 import Web3 from 'web3'
+import Navbar from './components/Navbar'
 import KryptoBird from './abis/KryptoBirdz.json'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
-  let accounts;
+  const [accountDetail, setAccountDetail] = useState({ accounts: null, networkId: null });
+
   useEffect(() => {
-    
+
     async function LoadWeb3() {
       const provider = await detectEthereumProvider();
       if (provider) {
@@ -21,23 +24,35 @@ function App() {
     }
 
     async function LoadBlockchainData() {
-      console.log(await window.web3);
+      // console.log(await window.web3);
       const acc = await window.web3.eth.getAccounts();
-      return acc;
+      const networkId = await window.web3.eth.net.getId();
+      return [acc[0], networkId];
     }
 
     async function call() {
-      const x = await LoadWeb3();
-      accounts = await LoadBlockchainData();
-      console.log(accounts);
+      await LoadWeb3();
+      let data = await LoadBlockchainData();
+      const updatedAccount = { accounts: data[0] }
+      const networkData = KryptoBird.networks[data[1]];
+      if (networkData) {
+        const abi = KryptoBird.abi;
+        const networkDataAdress = networkData.address;
+        const contract = new window.web3.eth.Contract(abi, networkDataAdress);
+        console.log(contract)
+      }
+      setAccountDetail((accountDetail) => ({ ...accountDetail, ...updatedAccount }))
     }
+
     call();
-  })
+  }, [])
 
   return (
     <>
+      <Navbar address={accountDetail.accounts} />
       <h1>Vite + React</h1>
       <p className="read-the-docs">
+        {accountDetail.accounts}
         Click on the Vite and React logos to learn more
       </p>
     </>
